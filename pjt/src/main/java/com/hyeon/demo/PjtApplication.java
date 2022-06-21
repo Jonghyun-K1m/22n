90ba5e36c8f6fd712c049ff74fe7788909abd754
hyun <jonghyun424@naver.com>
2022-06-21 17:15:17 +0900

    b

--- pjt/src/main/java/com/hyeon/demo/PjtApplication.java
+++ pjt/src/main/java/com/hyeon/demo/PjtApplication.java
@@ -2,7 +2,7 @@ package com.hyeon.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//asd
 @SpringBootApplication
 public class PjtApplication {
 
