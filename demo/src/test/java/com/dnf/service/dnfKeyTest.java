def6714d7cd98a5278af8805870d113c9700ac72
hyun <jonghyun424@naver.com>
2022-08-28 19:14:56 +0900

    add

--- demo/src/test/java/com/dnf/service/dnfKeyTest.java
+++ demo/src/test/java/com/dnf/service/dnfKeyTest.java
@@ -11,11 +11,21 @@ public class dnfKeyTest {
 	@Test
 	public void test()
 	{
-		dnfKey a = new dnfKey();
-		String abbb=a.resource;
-		String abc= a.get_driver();
+//		dnfKey a = new dnfKey();
+//		String abbb=a.resource;
+//		String abc= a.get_driver();
 		System.out.println("SDSD");
-		
+
+		double ac=1;
+		double ka=0.05;
+		double acs;
+		double etc=400;
+		for(int i=0;i<10;i++)
+		{
+			acs=ac*ka;
+			ac=ac+acs;
+			System.out.println(ac+etc);
+		}
 	}
 }
 
---
+++ jpahome/src/main/java/jpabook/jpashop/JpahomeApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpahomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpahomeApplication.class, args);
+	}
+
+}
