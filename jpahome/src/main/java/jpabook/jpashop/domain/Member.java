3e4d23680d2b91557a9c067aa978672fe307c1e0
hyun <jonghyun424@naver.com>
2022-08-29 17:18:18 +0900

    entity relation update

---
+++ jpahome/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+	@Id @GeneratedValue
+	@Column(name="merber_id")
+	private Long id;
+	private String name;
+	
+	@Embedded
+	private Address address;
+	
+	@OneToMany(mappedBy="member")//오더필드의 멤버에의해서 매핑댐, 을임
+	private List<Order> orders =new ArrayList<>();
+	
+	
+}
